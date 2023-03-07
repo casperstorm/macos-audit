d1bd725ba17ba05023e4b819ea057224417f1704
Casper Rogild Storm <casper+github@rogildstorm.com>
2023-03-07 22:13:59 +0100

    initial plist parsing

--- data/src/lib.rs
+++ data/src/lib.rs
@@ -1,8 +1,16 @@
-use std::{path::Path, process::Command};
+use plist::Plist;
+use std::{
+    io::{BufRead, BufReader, Cursor},
+    path::Path,
+    process::Command,
+};
 
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
 struct Entitlement {
+    #[serde(rename = "com.apple.security.device.camera")]
     key: String,
-    value: bool,
 }
 
 pub fn entitlements(path: &Path) {
@@ -14,7 +22,10 @@ pub fn entitlements(path: &Path) {
         .output()
         .expect("codesign command error")
         .stdout;
-    let payload = String::from_utf8(output).unwrap();
 
-    println!("{:?}", payload);
+    let file = Cursor::new(output);
+    let mut reader = BufReader::new(file);
+    let plist = Plist::from_reader(&mut reader).unwrap();
+
+    println!("{:?}", plist);
 }
